c64bc178c6cde37c25a1d9529de260e95460b5ee
haltroy <thehaltroy@gmail.com>
2022-04-20 23:47:32 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,258 @@
+﻿/*
+ *
+ * Name: Decimal To Hexadecimal converter
+ * Description: Converts a series of decimal numbers to hexadecimal numbers and vice-versa.
+ * Author: haltroy
+ * Language: C#
+ * Framework: .NET Core 3.1
+ * Type: Cross-platform Console Application
+ * Source Code: https://github.com/haltroy/dec2hex
+ *
+ */
+
+using System;
+using System.IO;
+
+namespace DecimalToHex
+{
+    internal class Program
+    {
+        private static string loc { get; set; } = string.Empty;
+        private static string inputPath { get; set; } = string.Empty;
+        private static string outputPath { get; set; } = string.Empty;
+        private static bool verbose { get; set; } = false;
+        private static bool inputIsString { get; set; } = false;
+        private static bool hex2dec { get; set; } = false;
+
+        private static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                loc = System.Globalization.CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
+
+                for (int i = 0; i < args.Length; i++)
+                {
+                    switch (args[i])
+                    {
+                        case "help":
+                        case "--help":
+                        case "-help":
+                        case "-h":
+                        case "/?":
+                            string help = GetHelp(loc);
+                            Console.WriteLine(help);
+                            return;
+
+                        case "-i":
+                            inputIsString = true;
+                            break;
+
+                        case "-v":
+                            verbose = true;
+                            break;
+
+                        case "-s":
+                            hex2dec = true;
+                            break;
+
+                        default:
+
+                            if (string.IsNullOrWhiteSpace(inputPath))
+                            {
+                                inputPath = inputIsString ? args[i].Replace("\"", "") : Path.GetFullPath(args[i].Replace("\"", ""));
+                            }
+                            else
+                            {
+                                outputPath = Path.GetFullPath(args[i].Replace("\"", ""));
+                            }
+                            break;
+                    }
+                }
+                if (!string.IsNullOrWhiteSpace(inputPath))
+                {
+                    if (inputIsString)
+                    {
+                        string outputString = hex2dec ? GetDec(inputPath) : GetHex(inputPath);
+                        WriteBack(outputString);
+                    }
+                    else
+                    {
+                        if (File.Exists(inputPath))
+                        {
+                            string inputString = string.Empty;
+                            using (FileStream fileStream = new FileStream(inputPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                            {
+                                using StreamReader sr = new StreamReader(fileStream);
+                                inputString = sr.ReadToEnd();
+                            }
+                            string outputString = hex2dec ? GetDec(inputString) : GetHex(inputString);
+                            WriteBack(outputString);
+                        }
+                        else
+                        {
+                            Console.Error.WriteLine(GetError(loc, 2), inputPath);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.Error.WriteLine(GetError(loc, 3));
+                }
+            }
+            else
+            {
+                Console.Error.WriteLine(GetError(loc, 1));
+            }
+        }
+
+        private static void WriteBack(string outputString)
+        {
+            if (!string.IsNullOrWhiteSpace(outputPath))
+            {
+                using FileStream outputStream = new FileStream(outputPath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+                using StreamWriter writer = new StreamWriter(outputStream);
+                writer.Write(outputString);
+            }
+            else
+            {
+                if (!verbose)
+                {
+                    Console.Error.WriteLine(GetError(loc, 0));
+                }
+            }
+        }
+
+        private static string GetHex(string inputString)
+        {
+            string[] decimals = inputString.Split(' ');
+            string outputString = string.Empty;
+            for (int i = 0; i < decimals.Length; i++)
+            {
+                var val = int.Parse(decimals[i]);
+                outputString += BitConverter.ToString(new byte[] { (byte)val }) + ' ';
+            }
+            if (verbose) { Console.WriteLine(outputString); }
+            outputString = outputString.Trim();
+            return outputString;
+        }
+
+        private static string GetDec(string inputString)
+        {
+            string[] decimals = inputString.Split(' ');
+            string outputString = string.Empty;
+            for (int i = 0; i < decimals.Length; i++)
+            {
+                var val = int.Parse(decimals[i]);
+                outputString += BitConverter.ToString(new byte[] { (byte)val }) + ' ';
+            }
+            if (verbose) { Console.WriteLine(outputString); }
+            outputString = outputString.Trim();
+            return outputString;
+        }
+
+        private static string GetError(string loc, int index)
+        {
+            switch (loc)
+            {
+                case "en":
+                default:
+                    switch (index)
+                    {
+                        case 0:
+                            return "Output path was empty.";
+
+                        case 1:
+                            return "No arguments given.";
+
+                        case 2:
+                            return "File \"{0}\" does not exists.";
+
+                        case 3:
+                            return "Input was empty.";
+
+                        default:
+                            return "Unknown error code \"" + index + "\".";
+                    }
+                case "tr":
+                    switch (index)
+                    {
+                        case 0:
+                            return "Çıkış dosyası konumu boştu.";
+
+                        case 1:
+                            return "Hiç bir argüman girilmemiş.";
+
+                        case 2:
+                            return "Dosya \"{0}\" bulunamadı.";
+
+                        case 3:
+                            return "Giriş argümanı boştu.";
+
+                        default:
+                            return "Bilinmeyen hata kodu \"" + index + "\".";
+                    }
+            }
+        }
+
+        private static string GetHelp(string loc)
+        {
+            switch (loc)
+            {
+                case "en":
+                default:
+                    return "Decimal <-> Hexadecimal Converter"
+                           + Environment.NewLine
+                           + "Converts Decimal to Hexadecimal and vice-versa."
+                           + Environment.NewLine
+                           + "Made by haltroy."
+                           + Environment.NewLine
+                           + "https://github.com/haltroy/dec2hex"
+                           + Environment.NewLine
+                           + ""
+                           + Environment.NewLine
+                           + "USAGE:"
+                           + Environment.NewLine
+                           + "d2h [-v] [-i] [-s] [help|--help|/?|-h|-help] [Input] [Output Path]"
+                           + Environment.NewLine
+                           + "-v                              Verboses the output to console."
+                           + Environment.NewLine
+                           + "-s                              Switches mode to Hexadecimal -> Decimal."
+                           + Environment.NewLine
+                           + "-i                              Changes Input from path to string."
+                           + Environment.NewLine
+                           + "help --help -help /? -h         Prints this output to console and quits."
+                           + Environment.NewLine
+                           + "[Input]                         File or string that contains the decimals seperated with a single space. Important."
+                           + Environment.NewLine
+                           + "[output Path]                   File that will contain the conversion result.";
+
+                case "tr":
+                    return "10'luk Sayı Sistemi <-> 16'lık Sayı Sistemi Çevirici"
+                           + Environment.NewLine
+                           + "10'luk Sayı sistemini 16'lık Sayı Sistemine veya ters şekilde çevirir."
+                           + Environment.NewLine
+                           + "haltroy tarafından yapılmıştır."
+                           + Environment.NewLine
+                           + "https://github.com/haltroy/dec2hex"
+                           + Environment.NewLine
+                           + ""
+                           + Environment.NewLine
+                           + "KULLANIM:"
+                           + Environment.NewLine
+                           + "d2h [-v] [-i] [-s] [help|--help|/?|-h|-help] [Giriş] [Çıkış Yolu]"
+                           + Environment.NewLine
+                           + "-v                              Çıkışı buraya yazar."
+                           + Environment.NewLine
+                           + "-s                              16'lık Sayı Sistemi -> 10'luk Sayı Sistemi moduna geçer."
+                           + Environment.NewLine
+                           + "-i                              Giriş yolunu giriş yazısına çevirir."
+                           + Environment.NewLine
+                           + "help --help -help /? -h         Bu menüyü yazıp kapanır."
+                           + Environment.NewLine
+                           + "[Giriş]                         10'luk yada 16'lık Sayı Sistemi ile yazılmış ve sayılar arasında boşluk olan yazı yada yazıyı içeren dosyanın konumunu belirler. Önemlidir."
+                           + Environment.NewLine
+                           + "[Çıkış Yolu]                    Çeviri sonucunnun yazılacağı dosya konumunu belirler.";
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# Decimal <-> Hexadecimal Converter
+
+A command-line tool for converting multiple decimal values to hexadecimal (or vice versa).
+
+## Features
+
+- Output to a file and/or print out the result
+- Input from either a file or a string.
+- Cross-platform and multi-language console application:
+   - English
+   - Turkish
+   - More Soon!
+
+## Requirements
+
+Can be used in a folder without installing it. Requirements are:
+
+- .NET Core 3.1 supported machine (see [this document](https://github.com/dotnet/core/blob/main/release-notes/3.1/3.1-supported-os.md))
+  - NOTE: The Official Document for .NET Core 3.1 might not include every operating system (such as Arch Linux), in order to learn if you operating system supports it or not, search on the internet (for example, I learned that Arch Linux supports .NET Core 3.1 by just typing `dotnet arch` into Google.)
+
+## Development
+
+Can be developed under any operating systems. The only requirement is that .NET SDK (in most package managers, `dotnet-sdk`) should be installed to the machine.
+
+To build, navigate to source folder from your terminal-of-choice and use `dotnet build` command to build.
+To build a release package, use `dotnet build -c Release` instead. 
+
+## Usage
+
+Can be used directly from binary file or from `dotnet` command.
+
+First, navigate to the parent folder of downlaoded file or source code and execute command that corresponds:
+
+ - Running from file : `d2h [Arguments]`
+ - Running from `dotnet` : `dotnet run -- [Arguments]`
+ 
+To see all arguments and information about this software, use `help` argument.
